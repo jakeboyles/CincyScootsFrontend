33fd7dc7e95dd4a15093c10b5596e97500834e13
Jake Boyles <jakeboyles@MacBook-Pro-8.local>
2018-10-06 13:01:16 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Map, TileLayer, Marker, Popup } from '../node_modules/react-leaflet/src';
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      birds: []
+    };
+  }
+
+  componentDidMount() {
+    var url = 'http://localhost:8080/birds'
+    fetch(url).then(
+      response => response.json()
+    ).then((birds)=>{
+      this.setState({
+        birds: birds.birds
+      })
+    }
+    );
+  }
   render() {
+    const position = [39.108774, -84.511449]
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {this.state.birds.map(bird=>{
+          return <Marker position={[bird.lat, bird.long]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        })}
+      </Map>
       </div>
     );
   }
